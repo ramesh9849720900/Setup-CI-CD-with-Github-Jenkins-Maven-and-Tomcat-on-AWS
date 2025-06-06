a4eb0c85ddf06106282f22316c4e6bc16f362e30
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-06-06 18:50:51 +0530

    Update MyTests.java

--- webapp/src/test/java/com/example/MyTests.java
+++ webapp/src/test/java/com/example/MyTests.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertTrue;
 public class MyTests {
 
     @Test
-    public void dummyTest() {
+    public void testAlwaysPasses() {
         assertTrue(true);
     }
 }
