b1867ccaa93d17e9efe990aa384a4a643d06f5ef
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-06-06 19:17:52 +0530

    Create MyTest.java

---
+++ webapp/src/test/java/com/example/MyTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyTest {
+
+    @Test
+    public void testExample() {
+        // Simple assertion to test if 2 + 2 equals 4
+        int result = 2 + 2;
+        assertEquals(4, result);
+    }
+}
